d6696a09af7599f0042951ea0c88d1311ce71962
VINITRAJ5 <31647341+VINITRAJ5@users.noreply.github.com>
2025-06-27 15:51:13 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,18 +1,18 @@
+import dynamic from 'next/dynamic';
+
+const Experience = dynamic(() => import('./components/homepage/experience'));
+const Education = dynamic(() => import('./components/homepage/education'));
+
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
-import Education from "./components/homepage/education";
-import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
-
- export default async function Home() {
-
-
+export default function Home() {
   return (
-    <div suppressHydrationWarning >
+    <div suppressHydrationWarning>
       <HeroSection />
       <AboutSection />
       <Experience />
@@ -21,5 +21,5 @@ import Skills from "./components/homepage/skills";
       <Education />
       <ContactSection />
     </div>
-  )
-};
+  );
+}
