5acc47b55e89b23bcdca6d084b5b9d596bbe02da
VINITRAJ5 <31647341+VINITRAJ5@users.noreply.github.com>
2025-06-27 15:38:35 +0530

    Update index.jsx

--- app/components/homepage/experience/index.jsx
+++ app/components/homepage/experience/index.jsx
@@ -1,13 +1,18 @@
 
 "use client";
 // @flow strict
+import dynamic from 'next/dynamic';
+
+const GlowCard = dynamic(() => import('../../helper/glow-card'), {
+  ssr: false,
+});
 
 import { experiences } from "@/utils/data/experience";
 import Image from "next/image";
 import { BsPersonWorkspace } from "react-icons/bs";
 import experience from '../../../assets/lottie/code.json';
 import AnimationLottie from "../../helper/animation-lottie";
-import GlowCard from "../../helper/glow-card";
+//import GlowCard from "../../helper/glow-card";
 
 
 function Experience() {
