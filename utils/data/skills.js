b92755ec52e1a57573ab6835bd34bb4b5da6b971
VINITRAJ5 <31647341+VINITRAJ5@users.noreply.github.com>
2025-10-07 19:43:04 +0530

    Update skills.js

--- utils/data/skills.js
+++ utils/data/skills.js
@@ -5,7 +5,6 @@ export const skillsData = [
   'React',
   'Next JS',
   'Tailwind',
-  'MySQL',
   'Git',
   'Bootstrap',
   'Figma',
@@ -15,6 +14,9 @@ export const skillsData = [
   'Flutter',
   'Node JS',
   'Premiere Pro',
+  'Typescript',
+  'AWS',
+  'Microsoft Office',
 ]
 
 // Choose your skills from below. Make sure it's in the same format and spelled correctly.
