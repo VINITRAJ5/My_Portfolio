bfe64939450ccf7f83bb82cfd7dbce26bd000b70
VINITRAJ5 <31647341+VINITRAJ5@users.noreply.github.com>
2026-09-09 16:21:41 +0530

    Update personal data for job designation and resume

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,8 +2,8 @@
 export const personalData = {
   name: "Vineet Raj",
   profile: '/profile.png',
-  designation: "Frontend Developer",
-  description: "Looking for Job in Front-end Developer,3 year of experience with MNC,Startup Company.Have Key Skills- (JavaScript,Typescript,React.js,Next.js,HTML5,CSS3,Tailwind,Redux,Testing,Jest,npm).Did B.Tech(CSE) from IPU.I am available for any kind of job opportunity that suits my skills and interests.",
+  designation: "Sr.Frontend Developer",
+  description: "Looking for Job in Sr.Frontend Developer,4+ year of experience with MNC,Startup Company.Have Key Skills- (JavaScript,Typescript,React.js,Next.js,HTML5,CSS3,Tailwind,Redux,Testing,Jest,npm).Did B.Tech(CSE) from IPU.I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'vinitrajiit2016@gmail.com',
   phone: '+917838282673',
   address: 'Delhi NCR',
@@ -14,5 +14,5 @@ export const personalData = {
   leetcode: "https://leetcode.com/u/vinitrajiit2016/",
   stackOverflow: 'https://stackoverflow.com/users/14255582/vineet-raj',
   devUsername: "",
-  resume: "https://drive.google.com/file/d/12wMfUuFLWpM_5ThaZPBNNK7ElgGTUDno/view?usp=sharing",
+  resume: "https://drive.google.com/file/d/1yvHkHFBa0zLk1jmf80KL2oX1NimCmG7n/view?usp=sharing",
 }
